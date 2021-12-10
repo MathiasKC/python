49f74beb73731f89f442dee0a104047a96276986
Mathias Kaempffer <kaempffer40@gmail.com>
2021-12-10 17:47:48 -0300

    cell class

--- elements/window.py
+++ elements/window.py
@@ -5,6 +5,23 @@ size = width, height = 600, 400
 black = 0, 0, 0
 white = 255, 255, 255
 screen = pygame.display.set_mode(size)
+r = 10;
+
+class Cell:
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
+
+
+    def draw(self):
+        top = pygame.draw.line(screen, white, (self.x, self.y), (self.x + self.r, self.y))
+        right = pygame.draw.line(screen, white, (self.x + self.r, self.y), (self.x + self.r, self.y + self.r))
+        bottom = pygame.draw.line(screen, white, (self.x + self.r, self.y + self.r), (self.x, self.y + self.r))
+        left = pygame.draw.line(screen, white, (self.x, self.y + self.r), (self.x, self.y))
+
+
+cell = Cell(0, 0, r)
 
 while True:
     #exit loop
@@ -14,6 +31,6 @@ while True:
     #fill screen
     screen.fill(black)
     #draw
-    pygame.draw.line(screen, white, (0, 0), (240, 250))
+    cell.draw()
     #update
     pygame.display.flip()
