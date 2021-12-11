2c15094c92c3cd9a6632ca35cadb28f8ebc23219
Mathias Kaempffer <kaempffer40@gmail.com>
2021-12-11 16:22:53 -0300

    remastered

--- elements/grid.py
+++ elements/grid.py
@@ -1,56 +1,38 @@
 import random
 import pygame
 import math
-
-height = 400
-width = 400
-
+width, height = 500, 500
+r = 10
+cols = math.floor(width/r)
+rows = math.floor(height/r)
+white = (255, 255, 255)
+black = (0, 0, 0)
 
 class Grid:
-    def __init__(self, x, y, r, screen, color):
-        self.x = x
-        self.y = y
-        self.r = r
+
+    def __init__(self, i, j, screen):
+        self.i = i
+        self.j = j
+        self.color = white
         self.screen = screen
-        self.color = color
-        rand = random.randrange(10)
-        self.type = False
-        if rand < 4:
-            self.type = True
-
-
-
 
     def draw(self):
-        top = pygame.draw.line(self.screen, self.color, (self.x, self.y), (self.x + self.r, self.y))
-        right = pygame.draw.line(self.screen, self.color, (self.x + self.r, self.y), (self.x + self.r, self.y + self.r))
-        bottom = pygame.draw.line(self.screen, self.color, (self.x + self.r, self.y + self.r), (self.x, self.y + self.r))
-        left = pygame.draw.line(self.screen, self.color, (self.x, self.y + self.r), (self.x, self.y))
+        x = self.i * r
+        y = self.j * r
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(x, y, r, r))
 
-    def set_mode(self):
-        if self.type:
-            self.x = width / 2
-            self.y = 0
-
-
-
-    def check_neighbors(self, grid_array):
-        if self.index(self.x, self.y) > len(grid_array):
-            return
-        top = grid_array[self.index(self.x, self.y - 1)]
-        right = grid_array[self.index(self.x + 1, self.y)]
-        bottom = grid_array[self.index(self.x, self.y + 1)]
-        left = grid_array[self.index(self.x - 1, self.y)]
-        #print("TOP=",top,"RIGHT=", right, "LEFT=", left, "BOTTOM=", bottom)
+    def check_neighbors(self, grid):
+        top = grid[index(self.i, self.j-1)]
+        right = grid[index(self.i + 1, self.j)]
+        bottom = grid[index(self.i, self.j + 1)]
+        left = grid[index(self.i - 1, self.j)]
 
 
 
-    def update(self):
-        if self.type:
-            pygame.draw.rect(self.screen, self.color, pygame.Rect(self.x, self.y, self.r, self.r))
 
 
-    def index(self, x, y):
-        if x < 0 or y < 0 or x > (width/self.r) - 1 or y > (height / self.r) - 1:
-            return -1
-        return int(x + y * width/self.r)
+def index(i,j):
+    if i < 0 or j < 0 or i > cols-1 or j > rows-1:
+        return -1;
+    else:
+        return i + j * cols;
--- elements/window.py
+++ elements/window.py
@@ -1,24 +1,29 @@
 import pygame, sys
 from grid import Grid
-
+import math
 
 #window and background
-size = width, height = 400, 400
+size = width, height = 500, 500
 black = 0, 0, 0
 white = 255, 255, 255
 screen = pygame.display.set_mode(size)
-r = 10;
-grid_array = []
+r = 10
+grid = []
+# frames per second setting
+FPS = 60
+fpsClock = pygame.time.Clock()
 
+cols = math.floor(width/r)
+rows = math.floor(height/r)
+
+for x in range(rows):
+    for y in range(cols):
+        cell = Grid(x, y, screen)
+        grid.append(cell)
 
 #setup:
 
-for x in range(0, width, r):
-    for y in range(0, height, r):
-        grid_array.append(Grid(x, y, r, screen, white))
-
-for grid in grid_array:
-    grid.set_mode()
+#events
 
 
 #main loop
@@ -27,15 +32,20 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+        #if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            #mouse click
+
+
+
     #fill screen
     screen.fill(black)
 
     #draw
-    for grid in grid_array:
-        #grid.draw()
-        grid.update()
-        grid.check_neighbors(grid_array)
-
+    for cell in grid:
+        cell.draw()
+        cell.check_neighbors(grid)
 
     #update
+
     pygame.display.flip()
+    fpsClock.tick(FPS)
