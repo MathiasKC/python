96f4ba5c3c2503e4f72e3b9c7adbf3f0d3c19fb0
Mathias Kaempffer <kaempffer40@gmail.com>
2021-12-13 01:50:40 -0300

    water added , not finished

--- elements/pixels.py
+++ elements/pixels.py
@@ -5,13 +5,15 @@ size = width, height = 500, 500
 r = 10
 cols = int(height/r)
 rows = int(width/r)
-
-black = 0, 0, 0
+#colors, elements
+sand = 255, 255, 0
 white = 255, 255, 255
+water = 0, 0, 255
 screen = pygame.display.set_mode(size)
 FPS = 40
 fpsClock = pygame.time.Clock()
 pressed = False
+liquid = False
 
 class Pixel:
     def __init__(self, i, j):
@@ -37,7 +39,12 @@ while True:
     if pressed:
         x, y = pygame.mouse.get_pos()
         print(x / r, y / r)
-        pixel_array[int(x / r)][int(y/r)].color = black
+        pixel_array[int(x / r)][int(y/r)].color = sand
+
+    if liquid:
+        x, y = pygame.mouse.get_pos()
+        print(x / r, y / r)
+        pixel_array[int(x / r)][int(y/r)].color = water
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -46,24 +53,54 @@ while True:
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 pressed = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            liquid = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 3:
+                liquid = False
 
-    screen.fill(black)
+
+    screen.fill(white)
     for x in range(cols):
         for y in range(rows):
             pixel_array[x][y].draw()
     for x in range(cols):
         for y in range(rows):
             if y < cols - 1:
-                if pixel_array[x][y].color == black and pixel_array[x][y + 1].color == white:
-                    pixel_array[x][y + 1].color = black
+                #SAND PHYSICS
+                if pixel_array[x][y].color == sand and pixel_array[x][y + 1].color == white:
+                    pixel_array[x][y + 1].color = sand
                     pixel_array[x][y].color = white
 
                 if x < rows - 1:
-                    if pixel_array[x][y].color == black and pixel_array[x][y + 1].color == black and pixel_array[x - 1][y + 1].color == white:
-                        pixel_array[x - 1][y + 1].color = black
+                    if pixel_array[x][y].color == sand and pixel_array[x][y + 1].color == sand and pixel_array[x - 1][y + 1].color == white:
+                        pixel_array[x - 1][y + 1].color = sand
                         pixel_array[x][y].color = white
-                    if pixel_array[x][y].color == black and pixel_array[x][y + 1].color == black and pixel_array[x - 1][y + 1].color == black and pixel_array[x + 1][y + 1].color == white:
-                        pixel_array[x + 1][y + 1].color = black
+
+                    if pixel_array[x][y].color == sand and pixel_array[x][y + 1].color == sand and pixel_array[x - 1][y + 1].color == sand and pixel_array[x + 1][y + 1].color == white:
+                        pixel_array[x + 1][y + 1].color = sand
                         pixel_array[x][y].color = white
+
+                #WATER PHISICS
+                #falling
+                if pixel_array[x][y].color == water and pixel_array[x][y + 1].color == white:
+                    pixel_array[x][y + 1].color = water
+                    pixel_array[x][y].color = white
+                if x < rows - 1:
+                    #check left down
+                    if pixel_array[x][y].color == water and pixel_array[x][y + 1].color == sand and pixel_array[x - 1][y + 1].color == white:
+                        pixel_array[x - 1][y + 1].color = water
+                        pixel_array[x][y].color = white
+
+
+            #water lighter than sand
+            if pixel_array[x][y].color == water and pixel_array[x][y - 1].color == sand:
+                pixel_array[x][y - 1].color = water
+                pixel_array[x][y].color = sand
+
+
+
+
+
     pygame.display.flip()
     fpsClock.tick(FPS)
